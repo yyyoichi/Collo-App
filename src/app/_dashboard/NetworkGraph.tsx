8250380e7408064d12fde0132cd89d52aae33b54
yyyoichi <origamiichiro@gmail.com>
2023-10-25 12:10:46 +0000

    doc: network graph

--- src/app/_dashboard/NetworkGraph.tsx
+++ src/app/_dashboard/NetworkGraph.tsx
@@ -35,13 +35,22 @@ const LoadGraph = (props: NetworkGraphProps) => {
 
     useEffect(() => {
         if (props.ready) return;
+        /** ノード表示数上限 */
         const MAX_NODE = 100;
+        /** 標準ノードサイズ */
         const BASE_NODE = 3;
+        /** 最大ノードサイズ */
         const MAX_NODE_SIZE = 6;
+        /** 最大エッジサイズ */
         const MAX_EDGE_SIZE = 4;
 
         const graph = new Graph();
+        /** MAX_NODE以下の共起ペア */
         const pairs = props.collo.getSortedPairs().splice(0, MAX_NODE);
+        /** 
+         * 0 ~ MAX_EDGE_SIZE にedgeサイズをスケーリングする係数 
+         * サイズ = 標準サイズ(0) + 共起回数 * スケーリング(最大サイズ / 共起最大数)
+         * */
         const EDGE_SCALING = MAX_EDGE_SIZE / pairs[0].count;
 
         /**単語識別子と共起回数の合計値を持つ */
@@ -65,6 +74,7 @@ const LoadGraph = (props: NetworkGraphProps) => {
             if (!nodeStrength.isAddedNode(id2)) {
                 addNode(id2);
             }
+            // 単語の出現回数を加算する
             nodeStrength.add(id1, count);
             nodeStrength.add(id2, count);
 
@@ -73,7 +83,12 @@ const LoadGraph = (props: NetworkGraphProps) => {
                 size: count * EDGE_SCALING
             });
         })
+        /**
+        * 0 ~ MAX_NODE_SIZE にnodeサイズをスケーリングする係数 
+        * サイズ = 標準サイズ(BASE_NODE) + 合計共起回数 * スケーリング(最大サイズ / 合計共起最大数)
+        * */
         const NODE_SCALING = MAX_NODE_SIZE / nodeStrength.getMaxCount();
+        // add size
         graph.forEachNode((node, attr) => {
             attr.size = BASE_NODE + nodeStrength.get(node) * NODE_SCALING;
         });

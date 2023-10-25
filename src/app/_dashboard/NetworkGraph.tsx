ac81a94ea13832aa7bc8e4a13614bfaab7bdf02e
yyyoichi <origamiichiro@gmail.com>
2023-10-25 06:31:27 +0000

    feat: show network graph with hover-anime

--- src/app/_dashboard/NetworkGraph.tsx
+++ src/app/_dashboard/NetworkGraph.tsx
@@ -1,47 +1,90 @@
 "use client";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useColloState } from '@/hooks/useColloState';
-import { MultiDirectedGraph } from 'graphology';
-import { SigmaContainer, useLoadGraph } from '@react-sigma/core';
+import Graph from 'graphology';
+import { ControlsContainer, FullScreenControl, SearchControl, SigmaContainer, ZoomControl, useLoadGraph, useRegisterEvents, useSetSettings, useSigma } from '@react-sigma/core';
+import { LayoutForceAtlas2Control, useLayoutForceAtlas2 } from "@react-sigma/layout-forceatlas2";
 import '@react-sigma/core/lib/react-sigma.min.css';
+import { Attributes } from 'graphology-types';
 
 export type NetworkGraphProps = Pick<ReturnType<typeof useColloState>, 'collo' | 'ready'>;
 export const NetworkGraph = (props: NetworkGraphProps) => {
     return <>
-        <SigmaContainer style={{ height: '500px', width: '500px' }}>
+        <SigmaContainer style={{ height: "500px" }}>
             <LoadGraph {...props} />
+            <ControlsContainer position={"bottom-right"}>
+                <ZoomControl />
+                <FullScreenControl />
+                <LayoutForceAtlas2Control settings={{ settings: { slowDown: 10 } }} />
+            </ControlsContainer>
+            <ControlsContainer position={"top-right"}>
+                <SearchControl style={{ width: "200px" }} />
+            </ControlsContainer>
         </SigmaContainer>
     </>
 }
 
 const LoadGraph = (props: NetworkGraphProps) => {
+    const { assign } = useLayoutForceAtlas2();
+    const sigma = useSigma();
+    const registerEvents = useRegisterEvents();
     const loadGraph = useLoadGraph();
+    const setSettings = useSetSettings();
+    const [hoveredNode, setHoveredNode] = useState<string | null>(null);
 
     useEffect(() => {
         if (props.ready) return;
-        const graph = new MultiDirectedGraph();
+        const graph = new Graph();
         for (const [wordID, word] of props.collo.wordsMap.entries()) {
             graph.addNode(wordID, {
                 label: word,
+                size: 5,
                 x: Math.random() * 100,
                 y: Math.random() * 100,
             });
         }
         let edgeID = 0;
         for (const { id1, id2, count } of props.collo.generatorPairs()) {
-            graph.addEdgeWithKey(edgeID, id1, id2, { size: count });
+            graph.addEdgeWithKey(edgeID, id1, id2);
             edgeID++;
         }
-
-        // const positions = forceAtlas2(graph, {
-        //     iterations: 50,
-        //     settings: {
-        //         gravity: 10
-        //     }
-        // });
-        // forceAtlas2.assign(graph, 50);
         loadGraph(graph);
-    }, [loadGraph, props.ready, props.collo]);
+        assign();
+
+        registerEvents({
+            enterNode: (event) => setHoveredNode(event.node),
+            leaveNode: () => setHoveredNode(null),
+        });
+    }, [loadGraph, registerEvents, props.ready, props.collo, assign]);
+
+    useEffect(() => {
+        setSettings({
+            nodeReducer: (node, data) => {
+                const graph = sigma.getGraph();
+                const newData: Attributes = { ...data, highlighted: data.highlighted || false };
+                // user doesnot hover any node
+                if (!hoveredNode) return newData;
+
+                // hightligth hover node and related node
+                if (node === hoveredNode || graph.neighbors(hoveredNode).includes(node)) {
+                    newData.highlighted = true;
+                } else {
+                    newData.color = "#E2E2E2";
+                    newData.highlighted = false;
+                }
+                return newData;
+            },
+            edgeReducer: (edge, data) => {
+                const graph = sigma.getGraph();
+                const newData = { ...data, hidden: false };
+
+                if (hoveredNode && !graph.extremities(edge).includes(hoveredNode)) {
+                    newData.hidden = true;
+                }
+                return newData;
+            },
+        });
+    }, [hoveredNode, setSettings, sigma]);
 
     return null;
 };
--- src/app/_dashboard/StartButton.tsx
+++ src/app/_dashboard/StartButton.tsx
@@ -6,7 +6,7 @@ export const StartButton = (props: StartButtonProps) => {
     return <>
         <button
             className='bg-green-400 p-4 font-mono font-bold text-white'
-            onClick={() => props.requestCollo(new Date(2022, 3, 1), new Date(2022, 3, 30), '震災')}
+            onClick={() => props.requestCollo(new Date(2022, 3, 1), new Date(2022, 3, 2), '震災')}
         >
             GET
         </button>

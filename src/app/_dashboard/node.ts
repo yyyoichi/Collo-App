271671aea011c59c69e486e73b4c2e167a90c0d2
yyyoichi <origamiichiro@gmail.com>
2023-10-25 11:46:43 +0000

    fix: addednode is not undefined

--- src/app/_dashboard/node.ts
+++ src/app/_dashboard/node.ts
@@ -2,7 +2,7 @@ export class NodeStrength {
   /**単語識別子とその共起回数の総数を保持する */
   readonly wordsPairCount = new Map<string, number>();
   isAddedNode(wordID: string) {
-    return typeof this.wordsPairCount.get(wordID) === 'undefined';
+    return typeof this.wordsPairCount.get(wordID) !== 'undefined';
   }
   add(wordID: string, count: number) {
     this.wordsPairCount.set(wordID, (this.wordsPairCount.get(wordID) || 0) + count);
